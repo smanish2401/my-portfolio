61d9c8ac27cf87f31688236ee28edd20c3be8b06
manish sharma <smanish2401@gmail.com>
2024-02-23 10:40:51 +0530

    add hamburger for side bar

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ var typed = new Typed('#element', {
     strings: ['web designer','web developer'],
     typeSpeed: 70,
 });
+
+document.querySelector(".hamburger").addEventListener("click",()=>{
+    document.getElementById("list").style.left = '20%'
+})
+document.querySelector(".close").addEventListener("click",()=>{
+    document.getElementById("list").style.left = '110%'
+})
